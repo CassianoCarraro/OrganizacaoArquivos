5fabe61ee9dd595ac2e29cbacd5cf8115109a288
danielg2_champy@Hotmail.com <danielg2_champy@Hotmail.com>
2017-10-14 02:38:33 -0300

    Adicionado Organização de arquivo em acesso direto

---
+++ OrganizacaoArquivos/OAAcessoDireto.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+
+
+namespace OrganizacaoArquivos
+{
+   public class OAAcessoDireto : OrganizacaoArquivo
+    {
+        const int NUM_BLOCOS = 100;
+        const int NUM_REG = 10000;
+
+        public OAAcessoDireto(string caminhoArqDados) : base(caminhoArqDados)
+        {
+        }
+
+        public OAAcessoDireto(string caminhoArqDados, FileAccess permissaoAcesso) : base(caminhoArqDados, permissaoAcesso)
+        {
+        }
+
+        public override object consultar(object registro, string attrId)
+        {
+            Int32 tamanhoRegistro = ((Registro)registro).Tamanho;
+            Int32 idRegistroTmp;
+            Colaborador colaboradorAux = (Colaborador)registro;
+            Int32 idRegistro = colaboradorAux.Numero;
+            Object registroTmp;
+
+            long hashGerado = idRegistro % NUM_BLOCOS;
+            long posInicial = hashGerado * NUM_REG;
+            long pos = 0;
+
+            dados.posicionar(0);
+
+            if(hashGerado < 0 || hashGerado > 100)
+            {
+                return null;
+            }
+
+            while (pos < NUM_REG)
+            {
+                dados.posicionar(posInicial);
+                registroTmp = dados.obterResgistro();
+                if (registroTmp == null)
+                {
+                    break;
+                }
+                else
+                {
+                    idRegistroTmp = (Int32)obterPropriedade(registroTmp, attrId);
+
+
+                    if (idRegistro == idRegistroTmp)
+                    {
+                        return registroTmp;
+                    }
+                    else
+                    {
+                        dados.avancar(pos * ((Registro)registro).Tamanho);
+                    }
+                    pos++;
+                }
+
+                
+            }
+
+            return null;
+        }
+
+        public override void inserir(object registro, string attrId)
+        {
+            Int32 idRegistro = (Int32)obterPropriedade(registro, attrId);
+            int hashGerado = idRegistro % NUM_BLOCOS;
+
+            if (hashGerado >= 0 && hashGerado <= NUM_BLOCOS)
+            {
+
+
+                long posInicial = hashGerado * NUM_REG;
+                dados.posicionar(posInicial);
+                Object registroArq = dados.obterResgistro();
+                if (registroArq == null)
+                {
+                    dados.gravar(registro);
+                }
+                else
+                {
+                    Int32 idReg = (Int32)obterPropriedade(registro, attrId);
+                    for (int i = 0; i < NUM_REG; i++)
+                    {
+                        Int32 idRegArq = (Int32)obterPropriedade(registroArq, attrId);
+
+                        if (idReg <= idRegArq)
+                        {
+                            dados.gravar(registro, dados.Pos - ((Registro)registro).Tamanho, ((Registro)registro).Tamanho);
+
+                            break;
+                        }
+                        else
+                        {
+                            posInicial += ((Registro)registro).Tamanho;
+                            dados.posicionar(posInicial);
+                            registroArq = dados.obterResgistro();
+                            if (registroArq == null)
+                            {
+                                dados.gravar(registro);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OrganizacaoArquivosTests/OAAcessoDiretoTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+
+namespace OrganizacaoArquivos.Tests
+{
+    [TestClass]
+    public class OAAcessoDiretoTests
+    {
+        private const String ARQ_DADOS_TESTE = "C:/Users/daniel/Desktop/organArq/acesso_direto_teste";
+
+        [TestMethod]
+        public void consultarTest()
+        {
+            Cleanup();
+
+            gerarArquivo(3, ARQ_DADOS_TESTE);
+            OAAcessoDireto oaAcessoDireto = new OAAcessoDireto(ARQ_DADOS_TESTE, FileAccess.Read);
+
+            Colaborador colaborador0 = new Colaborador(0);
+            Colaborador colaborador1 = new Colaborador(1);
+            Colaborador colaborador2 = new Colaborador(2);
+            Colaborador colaborador3 = new Colaborador(3);
+            Colaborador colaboradorm1 = new Colaborador(-1);
+
+            Colaborador colaboradorConsulta;
+
+            colaboradorConsulta = (Colaborador)oaAcessoDireto.consultar(colaborador0, "Numero");
+            Assert.AreEqual(colaboradorConsulta.Numero, colaborador0.Numero);
+
+            colaboradorConsulta = (Colaborador)oaAcessoDireto.consultar(colaborador1, "Numero");
+            Assert.AreEqual(colaboradorConsulta.Numero, colaborador1.Numero);
+
+            colaboradorConsulta = (Colaborador)oaAcessoDireto.consultar(colaborador2, "Numero");
+            Assert.AreEqual(colaboradorConsulta.Numero, colaborador2.Numero);
+
+            colaboradorConsulta = (Colaborador)oaAcessoDireto.consultar(colaborador3, "Numero");
+            Assert.AreEqual(colaboradorConsulta, null);
+
+            colaboradorConsulta = (Colaborador)oaAcessoDireto.consultar(colaboradorm1, "Numero");
+            Assert.AreEqual(colaboradorConsulta, null);
+
+            oaAcessoDireto.finalizar();
+        }
+
+        [TestMethod]
+        public void inserirTest()
+        {
+            Cleanup();
+
+            Int32 qtdReg = 100;
+            gerarArquivo(qtdReg, ARQ_DADOS_TESTE);
+
+            OAAcessoDireto oaAcessoDireto = new OAAcessoDireto(ARQ_DADOS_TESTE, FileAccess.Read);
+            Random aleatorio = new Random(5);
+
+            for (int i = 0; i < qtdReg; i++)
+            {
+                Colaborador colaborador = new Colaborador(i, "João " + i, aleatorio.Next(16, 65), aleatorio.NextDouble() * 1000);
+                Colaborador colaboradorBusca = (Colaborador)oaAcessoDireto.consultar(colaborador, "Numero");
+
+                Assert.AreEqual(colaborador.Numero, colaboradorBusca.Numero);
+                Assert.AreEqual(colaborador.Nome, colaboradorBusca.Nome);
+                Assert.AreEqual(colaborador.Salario, colaboradorBusca.Salario);
+            }
+
+            oaAcessoDireto.finalizar();
+        }
+
+        private void gerarArquivo(Int32 qtdReg, String caminhoArq)
+        {
+            OAAcessoDireto oaAcessoDireto = new OAAcessoDireto(caminhoArq);
+            Random aleatorio = new Random(5);
+
+            for (int i = 0; i < qtdReg; i++)
+            {
+                Colaborador colaborador = new Colaborador(i, "João " + i, aleatorio.Next(16, 65), aleatorio.NextDouble() * 1000);
+                oaAcessoDireto.inserir(colaborador, "Numero");
+            }
+
+            oaAcessoDireto.finalizar();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            File.Delete(ARQ_DADOS_TESTE);
+        }
+    }
+}
+
